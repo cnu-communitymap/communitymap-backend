8390201bcd76e8d579d8eb944cc9532ca34167b9
HaegyeongKim <Haegyeon.kim01@gmail.com>
2023-02-06 13:35:45 +0900

    [Modified] Comment

--- src/main/java/com/swacademy/mapcommunity/data/CommentRepository.java
+++ src/main/java/com/swacademy/mapcommunity/data/CommentRepository.java
@@ -2,14 +2,24 @@ package com.swacademy.mapcommunity.data;
 
 import com.swacademy.mapcommunity.domain.entity.Comment;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CommentRepository {
 
-    Comment registerComment(Comment comment);
+    Comment insert(Comment comment);
 
-    // @TODO UUID 생각
-    Comment getComment(UUID commentId);
+    // @ToDo update 제약 상속받는 클래스에 추가 . ex)아무 것도 입력 X 경우
+    Comment update(Comment comment);
+
+    void deleteById(UUID commentId);
+
+    /**
+     *
+     * @param commentId UUID
+     * @return Comment. null일 수도 있다.
+     */
+    Optional<Comment> getCommentById(UUID commentId);
 
     boolean upLike(UUID commentId);
 
--- src/main/java/com/swacademy/mapcommunity/domain/comment/CommentService.java
+++ src/main/java/com/swacademy/mapcommunity/domain/comment/CommentService.java
@@ -2,8 +2,10 @@ package com.swacademy.mapcommunity.domain.comment;
 
 import com.swacademy.mapcommunity.data.CommentRepository;
 import com.swacademy.mapcommunity.domain.entity.Comment;
+import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.UUID;
 @Service
 public class CommentService {
@@ -13,17 +15,55 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    Comment registerComment(Comment comment){
-        return commentRepository.registerComment(comment);   //수정 필요
+    /**
+     *
+     * @param commentId  UUID
+     * @param registerdPostId UUID
+     * @param userId UUID
+     * @param commentContent not null. repository에서 not null처리
+     * @param commentDate Date
+     * @param like int, not negative
+     * @return Comment
+     * @return If failed ? return null : return Comment
+     */
+    @Nullable
+    public Comment registerComment(UUID commentId, UUID registerdPostId, UUID userId, String commentContent, Date commentDate, int like){
+        Comment comment = new Comment(commentId, registerdPostId, userId, commentContent, commentDate, like);
+        try{
+            return commentRepository.insert(comment);
+        } catch (RuntimeException e) {  //@Todo 예외 정의
+            return null;
+        }
     }
 
-    Comment getComment(UUID commentId){
-        return commentRepository.getComment(commentId);
+    @Nullable
+    public Comment getComment(UUID commentId){
+        try{
+            var comment = commentRepository.getCommentById(commentId);
+            if (comment.isPresent()) return comment.get();
+            else return null;
+        } catch (RuntimeException e) { //@Todo 예외 정의
+            return null;
+        }
     }
 
-    boolean upLike(UUID commentId){
-        return commentRepository.upLike(commentId);
+    Comment updateComment(Comment comment){
+        return comment;
     }
 
-    //@Todo 나중에....void deleteComment(){} 추가
+    //@Todo 반환 type void...? boolean...? 다른 것...?
+    void deleteCommentById(UUID commentId){
+        commentRepository.deleteById(commentId);
+    }
+
+    //@Todo 반환 type 다시 생각
+    /**
+     *
+     * @param commentId 존재유무 판별은 repository에서 throw던지도록 처리
+     * @return void로 하고 upLike를 repository에서 try catch 처리
+     */
+    void upLike(UUID commentId){
+        commentRepository.upLike(commentId);
+    }
+
 }
