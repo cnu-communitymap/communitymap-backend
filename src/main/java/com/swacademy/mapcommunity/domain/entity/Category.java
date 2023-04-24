c1e3fb217f81aedba9067ab49b96755f608abf67
haegyeong <haegyeong.kim01@gmail.com>
2023-04-24 20:44:22 +0900

    rename: Category

--- src/main/java/com/swacademy/mapcommunity/domain/entity/Category.java
+++ src/main/java/com/swacademy/mapcommunity/domain/entity/Category.java
@@ -1,5 +1,5 @@
 package com.swacademy.mapcommunity.domain.entity;
 
 public enum Category {
-    INFORMATION, FOOD, HOSPITAL, CHAT, FESTIVAL    //CHAT 잡담
+    INFO, FOOD, HOSPITAL, TALK, FESTIVAL    //CHAT 잡담
 }
