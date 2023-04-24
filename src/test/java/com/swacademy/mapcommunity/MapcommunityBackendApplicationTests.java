28efb5c23a36274cd67022fb94720781f733426a
haegyeong <haegyeong.kim01@gmail.com>
2023-04-24 13:58:03 +0900

    was 서버에 build test & 실행

--- src/test/java/com/swacademy/mapcommunity/MapcommunityBackendApplicationTests.java
+++ src/test/java/com/swacademy/mapcommunity/MapcommunityBackendApplicationTests.java
@@ -1,10 +1,15 @@
 package com.swacademy.mapcommunity;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = com.swacademy.mapcommunity.MapcommunityBackendApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("local")
 class MapcommunityBackendApplicationTests {
 
